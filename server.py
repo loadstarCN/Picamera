1671fe8c176c703f76537d38b9aa39b356c65be2
richard <richard@olive.fm>
2017-06-29 16:57:11 +0800

    添加简单的http服务器，返回内存中的图片内容

--- server.py
+++ server.py
@@ -14,6 +14,7 @@ BUFSIZE= 1024
 
 device_pic_time_dict={}
 device_pic_write_dict={}
+pic_data=None
 
 UPLOAD_FOLDER="upload"
 if not os.path.exists(UPLOAD_FOLDER):
@@ -21,55 +22,70 @@ if not os.path.exists(UPLOAD_FOLDER):
 
 class Servers(SRH):  
     def handle(self):  
+        global pic_data
         print u"客户端已连接—> \n",self.client_address
         fhead = self.request.recv(FILEINFO_SIZE)
-        d_id,pic_time,filesize=struct.unpack('IdI',fhead)
-        print(u"[头部信息] ID:{} 时间:{} 文件长度:{} \n".format(d_id,pic_time,filesize))
+        print(fhead)
+        if 'GET' in fhead:
+            print(u'照片请求')
+            fhead+=self.request.recv(1024)
+            if pic_data:
+                data=pic_data
+                self.request.sendall(data)
+            
 
-        #todo根据time信息判断是否为最新图片
-        last_time=device_pic_time_dict.get(d_id)
-        
-        if not last_time or last_time<pic_time:
-            device_pic_time_dict[d_id]=pic_time
-        
-            is_write=device_pic_write_dict.get(d_id)
-
-            if not is_write:
-                device_pic_write_dict[d_id]=True
-              
-                
-                filename=os.path.join(UPLOAD_FOLDER,'d_{}.jpg'.format(str(d_id)))
-
-                fp = open(filename,'wb')
-                restsize = filesize
-                print(u"开始接收文件... \n")
-                
-                while True:
-                    try:  
-                        data = self.request.recv(1024)  
-                        if not data:   
-                            break  
-                        
-                        #print("接收文件长度:{} \n".format(len(data)))
-                        fp.write(data)
-
-                    except Exception as ex:  
-                        print ex.message
-                        break
-
-                print(u"接收文件完毕,正在断开连接... \n")
-                fp.close() 
-                device_pic_write_dict[d_id]=False
-            else:
-                print(u"文件正在读写,终止操作 \n")
         else:
-            print(u"文件s数据过时,终止操作 \n")
-        print(device_pic_time_dict)
-        print(device_pic_write_dict)
+            d_id,pic_time,filesize=struct.unpack('IdI',fhead)
+            print(u"[头部信息] ID:{} 时间:{} 文件长度:{} \n".format(d_id,pic_time,filesize))
+
+            #todo根据time信息判断是否为最新图片
+            last_time=device_pic_time_dict.get(d_id)
+            
+            if not last_time or last_time<pic_time:
+                device_pic_time_dict[d_id]=pic_time
+            
+                is_write=device_pic_write_dict.get(d_id)
+
+                if not is_write:
+                    device_pic_write_dict[d_id]=True
+                  
+                    
+                    filename=os.path.join(UPLOAD_FOLDER,'d_{}.jpg'.format(str(d_id)))
+
+                    fp = open(filename,'wb')
+                    restsize = filesize
+                    print(u"开始接收文件... \n")
+                    pic_data=None
+                    while True:
+                        try:  
+                            data = self.request.recv(1024)  
+                            if not data:   
+                                break  
+                            
+                            #print("接收文件长度:{} \n".format(len(data)))
+                            fp.write(data)
+                            if not pic_data:
+                                pic_data=data
+                            else:
+                                pic_data+=data
+
+                        except Exception as ex:  
+                            print ex.message
+                            break
+
+                    print(u"接收文件完毕,正在断开连接... \n")
+                    fp.close() 
+                    device_pic_write_dict[d_id]=False
+                else:
+                    print(u"文件正在读写,终止操作 \n")
+            else:
+                print(u"文件s数据过时,终止操作 \n")
+            print(device_pic_time_dict)
+            print(device_pic_write_dict)
 
 
 
 
-print '服务器开始运行....'  
+print(u'服务器开始运行....')  
 server = SocketServer.ThreadingTCPServer(addr,Servers)  
 server.serve_forever()  
